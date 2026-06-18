97e6582470e95e48cc6f4312298b01c2595274c4
Juan Diego <juandiegoq21@gmail.com>
2026-06-17 20:07:29 -0500

    restart

--- 3.CODIGO/Pry_TransControl/pry_transcontrol/android/app/src/main/kotlin/com/example/pry_transcontrol/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pry_transcontrol
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- 3.CODIGO/Pry_TransControl/pry_transcontrol/lib/presentation/providers/theme_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final themeProviderByMode = StateProvider<bool>((ref) {
-  return false; // false = light, true = dark
-});
